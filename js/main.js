55bdc1cdc878d2863e1b9f6c33308e49160429ab
Felix.Khodakovskiy <felix.khodakovskiy@nexign.com>
2020-04-08 14:00:42 +1000

    join index & main js

--- js/main.js
+++ js/main.js
@@ -49,3 +49,14 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
     })
 }, !1);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const multimediaScript = document.createElement('script');
+    multimediaScript.setAttribute('src','./js/multimedia.js');
+    const buttonsContainer = document.querySelector('.buttons-wrap');
+
+    buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
+        '<button class="button">Нет</button>';
+    
+    document.querySelector('.multimedia').appendChild(multimediaScript);
+});
