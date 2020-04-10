d42d2c386f09bcab54c2359e53faea462d5fc158
Felix.Khodakovskiy <felix.khodakovskiy@nexign.com>
2020-04-10 11:49:10 +1000

    rollback settimeout

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
             fridgeInfoContainer.replaceWith(purchaseListContainer)
             buttonsContainer.style.display = "none";
         }
-     }, 500);
+     }, 400);
    
     document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function () {
         document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
